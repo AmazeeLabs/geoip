fe16dc5bfd2fd7fb6cdea9e5afc40f91e2e0c8ef
geoip.amazeelabs.com <geoip_amazeelabs_com@azweb1.nine.ch>
2013-04-19 14:30:09 +0200

    checking in existing codebase

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+	header("Cache-control: private");
+	header("Content-Type: text/javascript; charset=utf-8");
+		
+	$geoip_array = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+
+	echo "function geoip_country_code(){ return '$geoip_array[country_code]'; }
+	function geoip_country_name(){ return '$geoip_array[country_name]'; }
+	function geoip_region(){ return '$geoip_array[region]'; }
+	function geoip_city(){ return '$geoip_array[city]'; }
+	function geoip_postal_code(){ return '$geoip_array[postal_code]'; }
+	function geoip_latitude(){ return '$geoip_array[latitude]'; }
+	function geoip_longitude(){ return '$geoip_array[longitude]'; }
+	function geoip_city(){ return '$geoip_array[city]'; }";
