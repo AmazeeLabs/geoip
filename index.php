27f401895260c29bd6719c6574e58598746aaf07
geoip.amazeelabs.com <geoip_amazeelabs_com@amazeeweb1.nine.ch>
2013-05-09 10:29:35 +0200

    change for making it work behind varnish

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	header('Pragma: no-cache');
 	header("Content-Type: text/javascript; charset=utf-8");
 		
-	$geoip_array = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+	$geoip_array = geoip_record_by_name($_SERVER['HTTP_X_FORWARDED_FOR']);
 
 	echo "function geoip_country_code(){ return '$geoip_array[country_code]'; }
 	function geoip_country_name(){ return '$geoip_array[country_name]'; }
